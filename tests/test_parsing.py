37e2893b52bad6a299a810f58df64f0b01ec2302
ljnsn <82611987+ljnsn@users.noreply.github.com>
2023-05-02 21:21:55 +0200

    ✅ test: add more tests for parsing

---
+++ tests/test_parsing.py
@@ -0,0 +1,35 @@
+from typing import Any, Dict, Optional, Union
+
+import pytest
+from dsws_client.ds_response import DSDataResponse, DSSymbolResponseValue
+from dsws_client.exceptions import InvalidResponseError
+from dsws_client.parse import parse_response, process_string_value, process_symbol_value
+
+
+def test_invalid_response(invalid_response: Dict[str, Any]) -> None:
+    """Verify parsing an invalid response raises an error."""
+    response = DSDataResponse(**invalid_response)
+
+    with pytest.raises(InvalidResponseError):
+        parse_response(response)
+
+
+def test_process_response_invalid_value() -> None:
+    """Verify processing a value raises an exception if invalid."""
+    symbol_value = DSSymbolResponseValue(
+        Symbol="AAPL",
+        Currency="E",
+        Type=10,
+        Value=[1, 2, 3],
+    )
+
+    with pytest.raises(InvalidResponseError):
+        process_symbol_value({}, [], "field", [], symbol_value, process_strings=False)
+
+
+@pytest.mark.parametrize(
+    ("value", "expected"), [("NA", None), ("N", False), ("Y", True), ("TEST", "TEST")]
+)
+def test_process_string_value(value: str, expected: Optional[Union[str, bool]]) -> None:
+    """Verify strings are cast to correct type."""
+    assert process_string_value(value) == expected
